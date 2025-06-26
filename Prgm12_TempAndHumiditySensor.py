ec49cf9aef4c155515cc01aa96e2190946a7bc5c
AkhilsaiMamidala <akhilakhil47427@gmail.com>
2025-06-26 13:38:08 +0530

    Initial commit

---
+++ Prgm12_TempAndHumiditySensor.py
@@ -0,0 +1,22 @@
+# @Auth Xtrans Solutions Pvt. Ltd.
+# Program to test DHT11 sensor
+# Before executing this program intsall Adafruit_DHT11 Library using follwing command
+# sudo pip install Adafruit_DHT11
+# Connect RM19 to RM2
+ 
+import time
+import Adafruit_DHT
+
+#Set the type of sensor and the pin for sensor
+sensor = Adafruit_DHT.DHT11
+pin = 4
+
+
+while(1):
+    try:
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        print ("Humidity ="+str(humidity))
+        print ("Temperature ="+str(temperature))
+    except ValueError:
+        print ("Unable to read data")
+
